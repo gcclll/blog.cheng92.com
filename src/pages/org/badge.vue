de16d3533740c8afc153a3a35f4bdff925019d77
gcclll <gccll.love@gmail.com>
2022-09-26 14:42:12 +0800

    2022-09-26 14:42:12

--- src/pages/org/badge.vue
+++ src/pages/org/badge.vue
@@ -13,8 +13,12 @@ const badge = reactive({
   logoColor: '',
   messageLink: '',
   labelLink: '',
+  style: 'flat',
   url: '',
 })
+
+// badge style
+const items = ['plastic', 'flat', 'flat-square', 'for-the-badge', 'social']
 
 function buildBadgeUrl(badge: OrgBadgeType) {
   const url = 'https://img.shields.io/static'
@@ -22,11 +26,10 @@ function buildBadgeUrl(badge: OrgBadgeType) {
   const query = buildUrlParam(params as any, true)
   const leftLink = messageLink ? `&link=${messageLink}` : ''
   const rightLink = labelLink ? `&link=${labelLink}` : ''
-  return `${url}/v${schemaVersion}?${query}` + leftLink + rightLink
+  return `${url}/v${schemaVersion}?${query + leftLink + rightLink}`
 }
 
 function submit() {
-  console.log(badge)
   badge.url = ''
   badge.url = buildBadgeUrl(badge)
 }
@@ -39,22 +42,27 @@ onMounted(() => {
 <template>
   <v-card :class="classList.content.card">
     <v-card-text :class="classList.content.text">
-      <h1>Org Badge Demo</h1>
+      <h1 class="text-center text-3xl pb-4">Org Badge Demo</h1>
+      <p>对应file.org 中的格式：&lt;label | labelColor | message | color | / | logo | logoColor | / | messageLink | labelLink&gt;</p>
       <v-form v-model="valid">
         <v-container>
           <v-row>
-            <v-col cols="12" md="3">
+            <v-col cols="12" md="6">
               <v-text-field v-model="badge.label" label="Label" />
             </v-col>
-
-            <v-col cols="12" md="3">
+            <v-col cols="12" md="6">
               <v-text-field v-model="badge.labelColor" label="Label Color" />
             </v-col>
-            <v-col cols="12" md="3">
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="4">
               <v-text-field v-model="badge.message" label="Message" required />
             </v-col>
-            <v-col cols="12" md="3">
+            <v-col cols="12" md="4">
               <v-text-field v-model="badge.color" label="Message Color" />
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-select :items="items" v-model="badge.style" label="Badge Style" />
             </v-col>
           </v-row>
           <v-row>
@@ -64,12 +72,13 @@ onMounted(() => {
             <v-text-field v-model="badge.labelLink" label="Label Link" />
           </v-row>
           <v-row>
-            <v-btn type="primary" @click.prevent="submit">生成BADGE</v-btn>
+            <v-btn color="success" @click.prevent="submit">生成BADGE</v-btn>
           </v-row>
         </v-container>
       </v-form>
 
-      <p>{{ badge.url }}</p>
+      <p class="text-blue mb-2">{{ badge.url }}</p>
+      结果：
       <OrgBadgeComp :data="badge" />
     </v-card-text>
   </v-card>
